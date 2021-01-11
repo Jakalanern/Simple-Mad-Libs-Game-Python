e9b3ee86c023848d0ddabffed9f6c2d6e3e13829
Jakalanern <77250250+Jakalanern@users.noreply.github.com>
2021-01-10 16:59:36 -0800

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+print("### MAD LIBS GAME ###")
+i1 = input("Enter a singular noun: ")
+i2 = input("Enter a verb: ")
+i3 = input("Enter an adjective: ")
+i4 = input("Enter a plural noun: ")
+
+print("There once was a {}, that really liked to {}. Liked to {} for all the {} {}.".format(i1.lower(), i2.lower(), i2.lower(), i3.lower(), i4.lower()))
